bbd1ba79d8b73f554406c6696b7298e2fcd9496b
Artur Ballod <artur.ballod@gmail.com>
2023-10-09 14:50:20 +0200

    @final

--- dev/static/js/slider.js
+++ dev/static/js/slider.js
@@ -12,7 +12,7 @@ const swiper2 = new Swiper('.map-popup__slider', {
     spaceBetween: 0,
     speed: 500,
     pagination: {
-        el:'.swiper-pagination'
+        el: '.swiper-pagination'
     },
     navigation: false,
     grabCursor: true,
@@ -25,10 +25,30 @@ const swiper3 = new Swiper('.map__nav', {
     grabCursor: true,
     slidesOffsetAfter: 20,
     slidesOffsetBefore: 20,
+    
 });
 
+var slides = document.querySelectorAll('.map__nav .swiper-slide');
+for (var i = 0; i < slides.length; i++) {
+    slides[i].addEventListener('click', function() {
+        // Remove is-active class from all slides
+        for (var j = 0; j < slides.length; j++) {
+          slides[j].classList.remove('is-active');
+        }
+        
+        // Add is-active class to clicked slide
+        this.classList.add('is-active');
+        
+        // Get index of clicked slide
+        var clickedIndex = Array.prototype.indexOf.call(slides, this);
+        
+        // Slide to clicked slide
+        swiper3.slideTo(clickedIndex);
+      });
+}
+
 var thisSwiper = [];
-$('.map__content-slider').each(function(i) {
+$('.map__content-slider').each(function (i) {
     var this_ID = $(this).attr('id');
 
     thisSwiper[i] = new Swiper('#' + this_ID, {
@@ -36,7 +56,7 @@ $('.map__content-slider').each(function(i) {
         spaceBetween: 0,
         speed: 500,
         pagination: {
-            el:'.swiper-pagination'
+            el: '.swiper-pagination'
         },
         navigation: false,
         grabCursor: true,
